cdd9a2a7719c527dbe242490b27dccbd5330e19d
wevs <68214494+dopewevmond@users.noreply.github.com>
2023-03-10 17:23:24 +0000

    fix: right sidebar should be hidden completely by default

--- src/components/RightSideBar.tsx
+++ src/components/RightSideBar.tsx
@@ -13,7 +13,7 @@ export const RightSideBar: React.FC<SideBarProps> = ({
   return (
     <div
       className={`${
-        isVisible ? "translate-x-0" : "translate-x-[1000px]"
+        isVisible ? "translate-x-0" : "translate-x-[calc(100vw)]"
       } slide-transition h-screen overflow-y-auto absolute top-0 right-0 z-10 bg-gray-100 dark:bg-gray-800 text-gray-900 dark:text-white w-[calc(100vw_-_300px)] lg:w-[calc(100vw_-_400px)] xl:w-[calc(100vw_-_500px)]`}
     >
       {children}
